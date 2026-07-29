0f751cf9935a3e0614a27ef9f768105a0f7646e6
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-29 22:24:05 +0530

    Implement rotateRight function for linked list

--- Rotate List/rotate-list-brute.cpp
+++ Rotate List/rotate-list-brute.cpp
@@ -1,1 +1,37 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        ListNode* temp=head;
+        if(head == NULL || head->next == NULL || k == 0) return head;
+        int cnt=1;
+        while (temp->next) {
+            cnt++;
+            temp=temp->next;
+        }
+        ListNode* dummy=head;
+        while (k>cnt) k=k-cnt;
+        if (cnt==k) return head;
+        int n=cnt-k;
+        while (dummy) {
+            if (n==1) {
+                ListNode* ans=dummy->next;
+                dummy->next=nullptr;
+                temp->next=head;
+                return ans;
+            }
+            n--;
+            dummy=dummy->next;
+        }
+        return head;
+    }
+};
